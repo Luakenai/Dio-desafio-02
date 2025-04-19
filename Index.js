0305c13dadb189d34312d30a1d6b296c71b3e7cf
Lua Jaxuka Morel <68887087+Luakenai@users.noreply.github.com>
2025-04-18 21:41:08 -0300

    Update Index.js
    Código feito.

--- Index.js
+++ Index.js
@@ -1,1 +1,26 @@
 
+function calcularRanks(vitoriasRankeadas, derrotasDoJogo) {
+    let saldoVitorias = vitoriasRankeadas - derrotasDoJogo;
+
+    let nivel = "";
+
+    if (vitoriasRankeadas <= 10) {
+        nivel = "Ferro";
+    } else if (vitoriasRankeadas <= 20) {
+        nivel = "Bronze";
+    } else if (vitoriasRankeadas <= 50) {
+        nivel = "Prata";
+    } else if (vitoriasRankeadas <= 80) {
+        nivel = "Ouro";
+    } else if (vitoriasRankeadas <= 90) {
+        nivel = "Diamante";
+    } else if (vitoriasRankeadas <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    console.log("O Herói tem um saldo de " + saldoVitorias + " vitórias e está no nível de " + nivel);
+}
+
+calcularRanks(50, 20);
